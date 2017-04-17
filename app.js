cda542bfe9b5886bc676f2369a389281eb6e6066
Daniel D Pires <danddpp@gmail.com>
2017-04-17 00:04:13 -0300

    17-04-17

--- app.js
+++ app.js
@@ -1269,6 +1269,6 @@ passport.use(new LocalStrategy({
 
 
 //initialize server
-server.listen(3000, function() {
-	console.log('Server on http://localhost:3000');
+server.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
